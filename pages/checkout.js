02097dc710467fff2d54ed95f97f22682b1f5f43
mannycolon <colonmanuel7@gmail.com>
2018-11-10 18:37:30 -0500

    Added Checkout Page

---
+++ pages/checkout.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+// core components
+import Layout from '../components/Layout'
+
+class CheckoutPage extends Component {
+  render() {
+    return (
+      <Layout>
+        <div>CheckoutPage</div>
+      </Layout>
+    )
+  }
+}
+
+export default CheckoutPage;
